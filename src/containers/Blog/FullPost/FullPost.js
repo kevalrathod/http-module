bde86929d121175cc5dea515741a3f86a138fe82
kevalrathod <kevalrathod1111@gmail.com>
2018-05-28 20:40:19 +0530

    third commit

--- src/containers/Blog/FullPost/FullPost.js
+++ src/containers/Blog/FullPost/FullPost.js
@@ -8,10 +8,19 @@ class FullPost extends Component {
         loadedPost: null
     }
 
-    componentDidUpdate () {
-        if ( this.props.id ) {
-            if ( !this.state.loadedPost || (this.state.loadedPost && this.state.loadedPost.id !== this.props.id) ) {
-                axios.get( 'https://jsonplaceholder.typicode.com/posts/' + this.props.id )
+    componentDidMount () {
+        console.log(this.props);
+        this.loadData();
+    }
+
+    componentDidUpdate() {
+        this.loadData();
+    }
+
+    loadData () {
+        if ( this.props.match.params.id ) {
+            if ( !this.state.loadedPost || (this.state.loadedPost && this.state.loadedPost.id !== +this.props.match.params.id) ) {
+                axios.get( '/posts/' + this.props.match.params.id )
                     .then( response => {
                         // console.log(response);
                         this.setState( { loadedPost: response.data } );
@@ -20,17 +29,16 @@ class FullPost extends Component {
         }
     }
 
-    deletePostHandler=()=>{
-        axios.delete('https://jsonplaceholder.typicode.com/posts/' + this.props.id)
-            .then(response=>{
+    deletePostHandler = () => {
+        axios.delete('/posts/' + this.props.match.params.id)
+            .then(response => {
                 console.log(response);
-                
             });
     }
 
     render () {
         let post = <p style={{ textAlign: 'center' }}>Please select a Post!</p>;
-        if ( this.props.id ) {
+        if ( this.props.match.params.id ) {
             post = <p style={{ textAlign: 'center' }}>Loading...!</p>;
         }
         if ( this.state.loadedPost ) {
@@ -39,7 +47,7 @@ class FullPost extends Component {
                     <h1>{this.state.loadedPost.title}</h1>
                     <p>{this.state.loadedPost.body}</p>
                     <div className="Edit">
-                        <button className="Delete" onClick={this.deletePostHandler}>Delete</button>
+                        <button onClick={this.deletePostHandler} className="Delete">Delete</button>
                     </div>
                 </div>
 
